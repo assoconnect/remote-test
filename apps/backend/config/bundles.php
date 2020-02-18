a23af6a562f99319dd9965646eebd49b20e7677d
Tristan Pouliquen <tristan.pouliquen@assoconnect.com>
2020-02-18 19:22:21 +0100

    CORS config for stub api

--- apps/backend/config/bundles.php
+++ apps/backend/config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Misd\PhoneNumberBundle\MisdPhoneNumberBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
